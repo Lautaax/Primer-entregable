0079a24458c7f2195223841955e1bdc995dcbaee
Lautaax <lautaa_97@outlook.com>
2023-02-21 01:44:35 -0300

    Cambios!

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-//Productos
-let product = new ProductPrin();
 
-product.getProduct() ;
+class ProductsPrin {
+  constructor() {
+    this.products = [];
+  }
 
-product.getProduct("PRUEBA")
-product.getProduct(1,"Anana", 10, "Es amarilla adentro", "Nohay","Sin foto" )
-product.getProduct(2,"MANZANA","Nohay", 150,"Sin foto",)
-product.getProduct(3,  "Sin foto")
-product.getProduct(4,"Mandarina", 30 ,"Es naranja" , 10  ,"Sin foto")
-product.getProduct(4,"Mandarina", 30 ,"Es naranja" , 10  ,"Sin foto")
-product.getProduct(5,"Uva", 8 ,"Son bolitas" , 20  ,"Sin foto")
-product.getProduct(6,"Papa", 3 ,"Es Puro Almidon" , 16  ,"Sin foto")
-product.getProduct(7,"Banana", 15 ,"Es amarilla" , 10  ,"Sin foto")
-product.getProduct() ;
-product.getProduct(1,"Anana", 10, "Es amarilla adentro", "Nohay","Sin foto" )
-product.getProduct() ;
-product.getProductById(8);
-product.getProductById(1);
-product.getProductById(4);
-product.getProductById(15);
-
-
-
-
-
-class ProductPrin {
-    constructor() {
-        this.product = [];
-    }
-}
 
 addProduct = (Id, Name, Stock, Descript, Precio, Foto) => {
-    const productIndex = this.products.findIndex((product) => product.Id === Id)
-    if (!Id || !Name || !Stock || !Descript || !Precio || !Foto ) {
-        console.log("Error datos iguales")
-        return;
+  const productIndex = this.products.findIndex((product) => product.Id === Id);
+  if (!Id || !Name || !Stock || !Descript || !Precio || !Foto) {
+    console.log("Error datos iguales");
+    return;
+  }
+  if (productIndex === -1) {
+    const product = {
+      Id: this.products.length + 1,
+      Name,
+      Stock,
+      Descript,
+      Precio,
+      Image,
+    };
+    this.products.push(product);
+    console.log(`Su Producto ${Id} Fue agregado`);
+  } else {
+    console.log(`Producto con mismo  ${Id}`);
+  }
+}
+  getProducts = () => {
+    console.log(this.products);
+    return this.products;
+  };
+
+  getProductById = (productId) => {
+    const productsIdFound = this.products.findIndex(
+      (prod) => prod.Id === productId
+    );
+    if (productsIdFound === -1) {
+      console.log(`Su producto ${ productId} No se encuentra `);
+    } else {
+      console.log(`Le suministramos los datos de su producto con ID ${productId}`);
+      console.log(this.products[productsIdFound]);
     }
-    if (productIndex === -1) {
-        const product = {
-            Id: this.product.lenght + 1,
-            Name,
-            Stock,
-            Descript,
-            Precio,
-            Image,
-        };
-        this.product.push(product);
-        console.log(`Su Producto ${Id} Fue agregado`)
-    }
-    else { console.log(`Producto con mismo  ${Id}`);
-    }
+  };
 }
 
-getProduct = () => {
-    console.log (this.product)
-    return this.product
-}
 
-getProductById = (productId) => {
-const productIdFound = this.product.findIndex((prod) =>prod.Id === productId)
-if (productIdFound ===-1){
-console.log(`Su producto ${Id} No se encuentra `)
-} else {
-    console.log(`Le suministramos los datos de su producto con ID ${Id}`);
-    console.log(this.product[productIdFound]);
-}
-}
+//Productos
+let products = new ProductsPrin();
+
+products.getProducts();
+
+products.addProduct("PRUEBA");
+products.addProduct(1, "Anana", 10, "Es amarilla adentro", "Nohay", "Sin foto");
+products.addProduct(2, "MANZANA", "Nohay", 150, "Sin foto");
+products.addProduct(3, "Sin foto");
+products.addProduct(4, "Mandarina", 30, "Es naranja", 10, "Sin foto");
+products.addProduct(4, "Mandarina", 30, "Es naranja", 10, "Sin foto");
+products.addProduct(5, "Uva", 8, "Son bolitas", 20, "Sin foto");
+products.addProduct(6, "Papa", 3, "Es Puro Almidon", 16, "Sin foto");
+products.addProduct(7, "Banana", 15, "Es amarilla", 10, "Sin foto");
+products.getProducts();
+products.getProducts(1, "Anana", 10, "Es amarilla adentro", "Nohay", "Sin foto");
+products.getProducts();
+products.getProductById(8);
+products.getProductById(1);
+products.getProductById(4);
+products.getProductById(15);
